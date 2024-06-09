784a49acb97e84024c96be8ded770199659dcbf7
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-10 00:00:48 +0600

    Update BarChart.js

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,27 +1,88 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
+import { io } from 'socket.io-client';
 
-const BarChart = ({ data }) => {
+const BarChart = ({ initialData }) => {
+  const [data, setData] = useState(initialData);
   const ref = useRef();
+  const svgRef = useRef();
+  const zoomRef = useRef();
 
   useEffect(() => {
-    const svg = d3.select(ref.current)
+    const socket = io('http://localhost:4000'); // Replace with your WebSocket server URL
+    socket.on('data', (newData) => {
+      setData(newData);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current)
       .attr('width', 500)
       .attr('height', 500)
       .style('border', '1px solid black');
+
+    const zoom = d3.zoom()
+      .scaleExtent([1, 10])
+      .on('zoom', (event) => {
+        svg.attr('transform', event.transform);
+      });
+
+    svg.call(zoom);
+
+    zoomRef.current = zoom;
+  }, []);
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    svg.selectAll('*').remove();
+
+    const xScale = d3.scaleBand()
+      .domain(data.map((d, i) => i))
+      .range([0, 500])
+      .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([500, 0]);
 
     svg.selectAll('rect')
       .data(data)
       .enter()
       .append('rect')
-      .attr('x', (d, i) => i * 50)
-      .attr('y', d => 500 - d * 10)
-      .attr('width', 40)
-      .attr('height', d => d * 10)
+      .attr('x', (d, i) => xScale(i))
+      .attr('y', d => yScale(d))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => 500 - yScale(d))
       .attr('fill', 'blue');
+
+    svg.append('g')
+      .call(d3.axisLeft(yScale));
+
+    svg.append('g')
+      .attr('transform', 'translate(0,500)')
+      .call(d3.axisBottom(xScale));
   }, [data]);
 
-  return <svg ref={ref}></svg>;
+  const handleFilterChange = (e) => {
+    const filterValue = e.target.value;
+    const filteredData = initialData.filter(d => d >= filterValue);
+    setData(filteredData);
+  };
+
+  return (
+    <div>
+      <div>
+        <label>Filter data greater than: 
+          <input type="number" onChange={handleFilterChange} />
+        </label>
+      </div>
+      <svg ref={svgRef}></svg>
+    </div>
+  );
 };
 
 export default BarChart;
