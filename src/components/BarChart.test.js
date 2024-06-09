772af6fb19d8c33d0571c37d4fe06b8203195e9f
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-10 00:05:04 +0600

    Update BarChart.test.js

--- src/components/BarChart.test.js
+++ src/components/BarChart.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import BarChart from './BarChart';
+import '@testing-library/jest-dom/extend-expect';
 
 test('renders bar chart', () => {
   const data = [12, 5, 6, 7, 10, 9];
-  const { container } = render(<BarChart data={data} />);
+  const { container } = render(<BarChart initialData={data} />);
   expect(container.querySelector('svg')).toBeInTheDocument();
 });
