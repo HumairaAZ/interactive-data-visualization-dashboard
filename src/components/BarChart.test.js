58527479813d07d9e89a27cbc3abe1f5f7a109e4
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-10 00:12:02 +0600

    Update BarChart.test.js

--- src/components/BarChart.test.js
+++ src/components/BarChart.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import BarChart from './BarChart';
 import '@testing-library/jest-dom/extend-expect';
 
-test('renders bar chart', () => {
-  const data = [12, 5, 6, 7, 10, 9];
-  const { container } = render(<BarChart initialData={data} />);
-  expect(container.querySelector('svg')).toBeInTheDocument();
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([12, 5, 6, 7, 10, 9]),
+  })
+);
+
+jest.mock('socket.io-client', () => {
+  return {
+    io: () => ({
+      on: jest.fn(),
+      disconnect: jest.fn(),
+    }),
+  };
+});
+
+test('renders bar chart', async () => {
+  const { container } = render(<BarChart />);
+  await waitFor(() => expect(container.querySelector('svg')).toBeInTheDocument());
 });
