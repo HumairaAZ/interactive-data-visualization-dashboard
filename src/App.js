aad35c2cbd81baaf353ca2341dd6969932b72fed
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-09 22:46:23 +0600

    Update and rename App.js: to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import BarChart from './components/BarChart';
 
 function App() {
+  const [data] = useState([12, 5, 6, 7, 10, 9]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Interactive Data Visualization Dashboard</h1>
+        <BarChart data={data} />
       </header>
     </div>
   );
